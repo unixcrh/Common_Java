3b2ddb82182d2014e12a6215ec8e6c57f803a6c4
unknown <dong@.(none)>
2011-08-13 09:48:32 +0800

    add solrclient

---
+++ src/com/orange/common/solr/SolrClient.java
@@ -0,0 +1,53 @@
+package com.orange.common.solr;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.CommonsHttpSolrServer;
+
+public class SolrClient {
+	static final String SOLR_SERVER_URL = "http://localhost:8080/solr";
+	CommonsHttpSolrServer server;
+	
+	public static CommonsHttpSolrServer getSolrServer() {
+		return getInstance().server;
+	}
+	
+	public static boolean commit(){
+		try {
+			getInstance().server.commit();
+			return true;
+		} catch (SolrServerException e) {			
+			e.printStackTrace();
+			return false;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public static SolrClient getInstance(){     
+        return SingletonContainer.instance;     
+    }     
+	
+	private static class SingletonContainer{     
+        private static SolrClient instance = new SolrClient();     
+    }     
+	
+	private SolrClient(){
+		try {
+			server = new CommonsHttpSolrServer(SOLR_SERVER_URL);
+		} catch (MalformedURLException e) {
+			e.printStackTrace(); //TODO
+		}
+		server.setSoTimeout(1000); // socket read timeout
+		server.setConnectionTimeout(100);
+		server.setDefaultMaxConnectionsPerHost(100);
+		server.setMaxTotalConnections(100);
+		server.setFollowRedirects(false); // defaults to false
+		server.setAllowCompression(true);
+		server.setMaxRetries(1); // defaults to 0. > 1 not recommended.	
+	}
+	
+}
