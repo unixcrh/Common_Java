5c0e08c54d5565f80e0cdcfec9bb0c4296096e3e
GamyHuang <hguangm2009@gmail.com>
2012-05-21 14:34:52 +0800

    update apnServer

--- src/com/orange/common/apnsservice/AppApnsService.java
+++ src/com/orange/common/apnsservice/AppApnsService.java
@@ -1,66 +1,96 @@
 package com.orange.common.apnsservice;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
+
+import org.apache.cassandra.thrift.Cassandra.system_add_column_family_args;
 
 import com.notnoop.apns.APNS;
 import com.notnoop.apns.ApnsService;
 import com.notnoop.apns.PayloadBuilder;
 
-public class AppApnsService extends BasicService{
-    
-    int badge = 0;
-    String alertMessage = "";
-    String sound = "default";
-    HashMap<String, Object> userInfo;   
+public class AppApnsService extends BasicService {
 
-    public AppApnsService(String certificatePath, String password) {
-        super(certificatePath, password);
-    }
-    
-    public AppApnsService(ApnsService service) {
-        super(service);
-    }
-    
-    public static AppApnsService createService(String certificatePath, String password, String deviceToken, int badge,
-                                            String alertMessage, String sound, HashMap<String, Object> userInfo) {
-        
-        AppApnsService service = new AppApnsService(certificatePath, password);
-        service.deviceToken = deviceToken;
-        service.badge = badge;
-        service.alertMessage = alertMessage;
-        service.sound = sound;
-        service.userInfo = userInfo;
-        return service;
-    }
-    
-    public static AppApnsService createService(ApnsService service, String deviceToken, int badge,
-            String alertMessage, String sound, HashMap<String, Object> userInfo) {
+	int badge = 0;
+	String alertMessage = "";
+	String sound = "default";
+	
+	String localizeKey;
+	Collection<String>localizeValues;
+	
+	HashMap<String, Object> userInfo;
 
-        AppApnsService Appservice = new AppApnsService(service);
-        Appservice.deviceToken = deviceToken;
-        Appservice.badge = badge;
-        Appservice.alertMessage = alertMessage;
-        Appservice.sound = sound;
-        Appservice.userInfo = userInfo;
-        return Appservice;
-    }
-    
-    @Override
-    boolean setPayload() {
-        PayloadBuilder builder = APNS.newPayload()
-                .badge(badge)
-                .sound(sound)
-                .alertBody(alertMessage);
-        
-        // set customized info
-        if (userInfo != null){
-            builder.customFields(userInfo);
-        }
-        
-        payload = builder.build();
-        
-        return true;
-    }
+	public AppApnsService(String certificatePath, String password) {
+		super(certificatePath, password);
+	}
+
+	public AppApnsService(ApnsService service) {
+		super(service);
+	}
+
+	public static AppApnsService createService(String certificatePath,
+			String password, String deviceToken, int badge,
+			String alertMessage, String sound, HashMap<String, Object> userInfo) {
+
+		AppApnsService service = new AppApnsService(certificatePath, password);
+		service.deviceToken = deviceToken;
+		service.badge = badge;
+		service.alertMessage = alertMessage;
+		service.sound = sound;
+		service.userInfo = userInfo;
+		return service;
+	}
+
+	public static AppApnsService createService(String certificatePath,
+			String password, String deviceToken, int badge,
+			String localizeKey, List<String>localizeValues, String sound, HashMap<String, Object> userInfo) {
+
+		AppApnsService service = new AppApnsService(certificatePath, password);
+		service.deviceToken = deviceToken;
+		service.badge = badge;
+		service.localizeKey = localizeKey;
+		service.localizeValues = localizeValues;
+		service.sound = sound;
+		service.userInfo = userInfo;
+		return service;
+	}
+
+	public static AppApnsService createService(ApnsService service,
+			String deviceToken, int badge, String alertMessage, String sound,
+			HashMap<String, Object> userInfo) {
+
+		AppApnsService Appservice = new AppApnsService(service);
+		Appservice.deviceToken = deviceToken;
+		Appservice.badge = badge;
+		Appservice.alertMessage = alertMessage;
+		Appservice.sound = sound;
+		Appservice.userInfo = userInfo;
+		return Appservice;
+	}
+
+	@Override
+	boolean setPayload() {
+		PayloadBuilder builder = null;
+		if (alertMessage != null && alertMessage.length() != 0) {
+			builder = APNS.newPayload().badge(badge).sound(sound)
+			.alertBody(alertMessage);			
+		}else if(localizeKey != null && localizeKey.length() != 0)
+		{
+			builder = APNS.newPayload().badge(badge).sound(sound)
+			.localizedKey(localizeKey).localizedArguments(localizeValues);						
+		}else{
+			return false;
+		}
+		// set customized info
+		if (userInfo != null) {
+			builder.customFields(userInfo);
+		}
+
+		payload = builder.build();
+		System.out.println(builder.toString());
+
+		return true;
+	}
 
 }
